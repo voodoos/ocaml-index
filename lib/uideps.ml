a1602160aa147b31a1e2b99815d78130caad7bed
Ulysse Gérard <thevoodoos@gmail.com>
2022-02-11 16:18:34 +0100

    Use shapes to get definitions' uids

--- lib/uideps.ml
+++ lib/uideps.ml
@@ -1,4 +1,24 @@
 open File_format
+module Kind = Shape.Sig_component_kind
+
+module Shape_reduce = 
+  Shape_reduce.Make (struct
+    type env = Env.t
+    let fuel = 10
+    let persist_memo = true
+
+    let read_unit_shape ~unit_name = 
+      let cmt = String.concat "." [unit_name; "cmt"] in
+      match Cmt_format.read (Load_path.find_uncap cmt) with
+      | _, Some cmt_infos -> 
+        (* TODO should we complete the load_path ? *)
+        cmt_infos.cmt_impl_shape
+      | _, None | exception Not_found -> 
+        Printf.eprintf "Failed to load cmt: %s\n%!" cmt; 
+        None
+    let find_shape env id = Env.shape_of_path
+      ~namespace:Shape.Sig_component_kind.Module env (Pident id)
+  end) 
 
 type typedtree =
  | Interface of Typedtree.signature
@@ -14,17 +34,20 @@ let merge_tbl ~into tbl = Hashtbl.iter (add into) tbl
 
 let gather_uids tree =
   let tbl = Hashtbl.create 64 in
-  let add_to_tbl ~loc uid = 
-      add tbl uid (LocSet.singleton loc)
-  in
   let iterator env =
+    let add_to_tbl ~loc shape = 
+        match (Shape_reduce.reduce env shape).uid with
+        | Some uid -> add tbl uid (LocSet.singleton loc)
+        | None -> ()
+    in
     { Tast_iterator.default_iterator with
 
       expr =
         (fun sub ({ exp_desc; exp_loc; _} as e) ->
           begin match exp_desc with
-          | Texp_ident (_, _, { val_uid; _ }) ->
-            add_to_tbl ~loc:exp_loc val_uid
+          | Texp_ident (path, _, { val_uid=_; _ }) ->
+            let shape = Env.shape_of_path ~namespace:Kind.Value env path in
+            add_to_tbl ~loc:exp_loc shape
           | _ -> () end;
           Tast_iterator.default_iterator.expr sub e);
 
@@ -32,8 +55,8 @@ let gather_uids tree =
         (fun sub ({ mod_desc; mod_loc; _} as me) ->
           begin match mod_desc with
           | Tmod_ident (path, _lid) ->
-            let md = Env.find_module path env in
-            add_to_tbl ~loc:mod_loc md.md_uid
+            let shape = Env.shape_of_path ~namespace:Kind.Module env path in
+            add_to_tbl ~loc:mod_loc shape
           | _ -> () end;
           Tast_iterator.default_iterator.module_expr sub me);
 
@@ -41,8 +64,8 @@ let gather_uids tree =
         (fun sub ({ ctyp_desc; ctyp_loc; _} as me) ->
           begin match ctyp_desc with
           | Ttyp_constr (path, _lid, _ctyps) ->
-            let td = Env.find_type path env in
-            add_to_tbl ~loc:ctyp_loc td.type_uid
+            let shape = Env.shape_of_path ~namespace:Kind.Type env path in
+            add_to_tbl ~loc:ctyp_loc shape
           | _ -> () end;
           Tast_iterator.default_iterator.typ sub me);
 
@@ -59,7 +82,6 @@ let gather_uids tree =
   tbl
 
 let get_typedtree (cmt_infos : Cmt_format.cmt_infos) =
-  Load_path.init cmt_infos.cmt_loadpath;
   match cmt_infos.cmt_annots with
   | Interface s ->
     let sig_final_env = Envaux.env_of_only_summary s.sig_final_env in
@@ -78,7 +100,9 @@ let generate_one_aux ~input_file tree =
 
 let generate_one input_file =
   match Cmt_format.read input_file with
-  | _, Some cmt_infos -> begin match get_typedtree cmt_infos with
+  | _, Some cmt_infos -> 
+    Load_path.init cmt_infos.cmt_loadpath;
+    begin match get_typedtree cmt_infos with
     | Some tree -> generate_one_aux ~input_file tree
     | None -> (* todo log error *) ()
     end
--- tests/uideps.t
+++ tests/uideps.t
@@ -25,14 +25,14 @@
    uid: Foo.2; locs: File "main.ml", line 1, characters 16-21;
                      File "main.ml", line 2, characters 8-13
    uid: Bar.0; locs: File "main.ml", line 2, characters 16-21
-   uid: Stdlib.48; locs: File "main.ml", line 1, characters 14-15;
+   uid: Stdlib.55; locs: File "main.ml", line 1, characters 14-15;
                          File "main.ml", line 2, characters 14-15
    uid: Foo.0; locs: File "main.ml", line 3, characters 13-18}
 
   $ ocaml-uideps dump foo.uideps
   {uid: Foo.1; locs: File "foo.ml", line 3, characters 21-22
    uid: Bar.0; locs: File "foo.ml", line 3, characters 13-18
-   uid: Stdlib.48; locs: File "foo.ml", line 3, characters 11-12;
+   uid: Stdlib.55; locs: File "foo.ml", line 3, characters 11-12;
                          File "foo.ml", line 3, characters 19-20
    }
 
@@ -50,7 +50,7 @@
                      File "main.ml", line 1, characters 8-13
    uid: Bar.0; locs: File "foo.ml", line 3, characters 13-18;
                      File "main.ml", line 2, characters 16-21
-   uid: Stdlib.48; locs: File "foo.ml", line 3, characters 11-12;
+   uid: Stdlib.55; locs: File "foo.ml", line 3, characters 11-12;
                          File "foo.ml", line 3, characters 19-20;
                          File "main.ml", line 1, characters 14-15;
                          File "main.ml", line 2, characters 14-15
