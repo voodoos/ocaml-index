2a99c8527000f47da136e4d9ba2a352bee2f72ea
Ulysse Gérard <thevoodoos@gmail.com>
2022-02-11 14:53:23 +0100

    Usage tweak

--- bin/ocaml_uideps.ml
+++ bin/ocaml_uideps.ml
@@ -1,4 +1,6 @@
-type command = Process | Aggregate | Dump
+open Lib
+
+type command = Process | Aggregate | Dump | Usage
 exception Unknown_command of string
 exception Too_many_files
 exception Too_few_files
@@ -7,6 +9,7 @@ let cmd_to_string = function
   | Process -> "process-cmt"
   | Aggregate -> "aggregate"
   | Dump -> "dump"
+  | Usage -> "usage"
 
 let cmd_of_string = function
   | "process-cmt" -> Process
@@ -15,10 +18,10 @@ let cmd_of_string = function
   | s -> raise (Unknown_command s)
 
 let usage_msg = "ocaml-uideps process-cmt <cmt> [<cmt>] ...
-\nocaml-uideps aggregate <usage1> [<usage2>] ...
+\nocaml-uideps aggregate <uideps> [<uideps>] ...
 \nocaml-uideps dump <cmt>"
 
-let command = ref Process
+let command = ref Usage
 let input_files = ref []
 let output_file : string option ref = ref None
 
@@ -44,6 +47,7 @@ let () = try
   | Process, files -> Uideps.generate files
   | Dump, [file] -> File_format.(read ~file |> pp_payload Format.std_formatter)
   | Aggregate, _ -> Uideps.aggregate !input_files
+  | Usage, _ -> Format.print_string usage_msg
 with
   Unknown_command cmd -> Printf.eprintf "Unknown command %S." cmd; exit 2
   | Too_many_files ->
