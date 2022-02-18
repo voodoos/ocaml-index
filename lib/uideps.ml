9ee78e0db7da96518f29a78930a93b602319d055
Ulysse Gérard <thevoodoos@gmail.com>
2022-02-18 15:47:28 +0100

    Reuse memo accros CUs (make that optional) + always use local envs

--- lib/uideps.ml
+++ lib/uideps.ml
@@ -1,25 +1,6 @@
 open File_format
 module Kind = Shape.Sig_component_kind
 
-module Shape_reduce = 
-  Shape_reduce.Make (struct
-    type env = Env.t
-    let fuel = 10
-    let persist_memo = true
-
-    let read_unit_shape ~unit_name = 
-      let cmt = String.concat "." [unit_name; "cmt"] in
-      match Cmt_format.read (Load_path.find_uncap cmt) with
-      | _, Some cmt_infos -> 
-        (* TODO should we complete the load_path ? *)
-        cmt_infos.cmt_impl_shape
-      | _, None | exception Not_found -> 
-        Printf.eprintf "Failed to load cmt: %s\n%!" cmt; 
-        None
-    let find_shape env id = Env.shape_of_path
-      ~namespace:Shape.Sig_component_kind.Module env (Pident id)
-  end) 
-
 type typedtree =
  | Interface of Typedtree.signature
  | Implementation of Typedtree.structure
@@ -33,9 +14,30 @@ let add tbl uid locs =
 let merge_tbl ~into tbl = Hashtbl.iter (add into) tbl
 
 let gather_uids tree =
+  Log.debug "Gather UIDS";
   let tbl = Hashtbl.create 64 in
-  let iterator env =
-    let add_to_tbl ~loc shape = 
+  let  module Shape_reduce =
+    Shape_reduce.Make (struct
+      type env = Env.t
+      let fuel = 10
+      let persist_memo = true
+
+      let read_unit_shape ~unit_name =
+        Log.debug "Read unit shape: %s\n%!" unit_name;
+        let cmt = String.concat "." [unit_name; "cmt"] in
+        match Cmt_format.read (Load_path.find_uncap cmt) with
+        | _, Some cmt_infos ->
+          (* TODO should we complete the load_path ? *)
+          cmt_infos.cmt_impl_shape
+        | _, None | exception Not_found ->
+          Log.warn "Failed to load cmt: %s\n%!" cmt;
+          None
+
+      let find_shape env id = Env.shape_of_path
+        ~namespace:Shape.Sig_component_kind.Module env (Pident id)
+    end) in
+  let iterator =
+    let add_to_tbl ~env ~loc shape =
         match (Shape_reduce.reduce env shape).uid with
         | Some uid -> add tbl uid (LocSet.singleton loc)
         | None -> ()
@@ -43,41 +45,52 @@ let gather_uids tree =
     { Tast_iterator.default_iterator with
 
       expr =
-        (fun sub ({ exp_desc; exp_loc; _} as e) ->
+        (fun sub ({ exp_desc; exp_loc; exp_env = env; _} as e) ->
           begin match exp_desc with
           | Texp_ident (path, _, { val_uid=_; _ }) ->
-            let shape = Env.shape_of_path ~namespace:Kind.Value env path in
-            add_to_tbl ~loc:exp_loc shape
+            begin try
+              let shape = Env.shape_of_path ~namespace:Kind.Value env path in
+              add_to_tbl ~env ~loc:exp_loc shape
+            with Not_found ->
+              Log.warn "No shape for expr %a" Path.print path
+            end
           | _ -> () end;
           Tast_iterator.default_iterator.expr sub e);
 
       module_expr =
-        (fun sub ({ mod_desc; mod_loc; _} as me) ->
+        (fun sub ({ mod_desc; mod_loc; mod_env = env; _} as me) ->
           begin match mod_desc with
           | Tmod_ident (path, _lid) ->
-            let shape = Env.shape_of_path ~namespace:Kind.Module env path in
-            add_to_tbl ~loc:mod_loc shape
+            begin try
+              let shape = Env.shape_of_path ~namespace:Kind.Module env path in
+              add_to_tbl ~env ~loc:mod_loc shape
+            with Not_found ->
+              Log.warn "No shape for module %a\n%!" Path.print path
+            end
           | _ -> () end;
           Tast_iterator.default_iterator.module_expr sub me);
 
       typ =
-        (fun sub ({ ctyp_desc; ctyp_loc; _} as me) ->
+        (fun sub ({ ctyp_desc; ctyp_loc; ctyp_env = env;_} as me) ->
           begin match ctyp_desc with
           | Ttyp_constr (path, _lid, _ctyps) ->
-            let shape = Env.shape_of_path ~namespace:Kind.Type env path in
-            add_to_tbl ~loc:ctyp_loc shape
+            begin try
+              let shape = Env.shape_of_path ~namespace:Kind.Type env path in
+              add_to_tbl ~env ~loc:ctyp_loc shape
+            with Not_found ->
+              Log.warn "No shape for type %a at %a"
+                Path.print path
+                Location.print_loc ctyp_loc;
+              raise Not_found
+            end
           | _ -> () end;
           Tast_iterator.default_iterator.typ sub me);
 
     }
   in
   begin match tree with
-  | Interface signature ->
-    let iterator = iterator signature.sig_final_env in
-    iterator.signature iterator signature
-  | Implementation structure ->
-    let iterator = iterator structure.str_final_env in
-    iterator.structure iterator structure
+  | Interface signature -> iterator.signature iterator signature
+  | Implementation structure -> iterator.structure iterator structure
   end;
   tbl
 
