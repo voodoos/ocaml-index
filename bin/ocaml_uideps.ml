40712f48080dd23110c0742f7cc4551664f1ff6a
Ulysse Gérard <thevoodoos@gmail.com>
2023-03-21 18:43:02 -0300

    Move to cmdliner

--- bin/ocaml_uideps.ml
+++ bin/ocaml_uideps.ml
@@ -1,81 +1,105 @@
 open Lib
+open Cmdliner
 
-type command = Process | Aggregate | Dump | Usage
+module Common = struct
+  let set_log_level debug verbose continue =
+    Log.set_log_level Error;
+    if verbose then Log.set_log_level Warning;
+    if debug then Log.set_log_level Debug;
+    continue
 
-exception Unknown_command of string
-exception Too_many_files
-exception Too_few_files
+  let verbose =
+    let doc = "increase log verbosity" in
+    Arg.(value & flag & info [ "v"; "verbose" ] ~doc)
 
-let cmd_to_string = function
-  | Process -> "process-cmt"
-  | Aggregate -> "aggregate"
-  | Dump -> "dump"
-  | Usage -> "usage"
+  let debug =
+    let doc = "set maximum log verbosity" in
+    Arg.(value & flag & info [ "debug" ] ~doc)
 
-let cmd_of_string = function
-  | "process-cmt" -> Process
-  | "aggregate" -> Aggregate
-  | "dump" -> Dump
-  | s -> raise (Unknown_command s)
+  let with_log t = Term.(const set_log_level $ debug $ verbose $ t)
 
-let usage_msg =
-  "ocaml-uideps process-cmt <cmt> [<build-path>] ...\n\n\
-   ocaml-uideps aggregate -o <output> <uideps> [<uideps>] ...\n\n\
-   ocaml-uideps dump <cmt>"
+  let output_file =
+    let doc = "name of the generated index" in
+    Arg.(
+      value
+      & opt string (Arg.doc_quote "project.index")
+      & info [ "o"; "output-file" ] ~doc)
+end
 
-let command = ref Usage
-let verbose = ref false
-let debug = ref false
-let input_files = ref []
-let output_file = ref "workspace.uideps"
-let root = ref "."
-let store_shapes = ref false
+module Index = struct
+  let generate root output_file build_path file =
+    Uideps.generate ~root ~output_file ~build_path file
 
-let anon_fun =
-  let first = ref true in
-  fun filename ->
-    if !first then (
-      command := cmd_of_string filename;
-      first := false)
-    else input_files := filename :: !input_files
+  let root =
+    let doc = "if provided all locations will be appended to that path" in
+    Arg.(value & opt (some string) None & info [ "root" ] ~doc)
 
-let speclist =
-  [
-    ("--verbose", Arg.Set verbose, "Output log information");
-    ("--debug", Arg.Set debug, "Output debug information");
-    ("-o", Arg.Set_string output_file, "Set output file name");
-    ("--debug", Arg.Set debug, "Output debug information");
-    ("--root", Arg.Set_string root, "Root path");
-    ("--store-shapes", Arg.Set store_shapes, "Aggregate input-indexes shapes and store them\
-                                            along with the index");
-  ]
+  let cmt_file =
+    let doc = "the $(i, .cmt) file to be indexed" in
+    Arg.(required & pos 0 (some string) None & info [] ~doc)
 
-let () =
-  try
-    Arg.parse speclist anon_fun usage_msg;
-    Log.set_log_level Warning;
-    if !verbose then Log.set_log_level Debug;
-    if !debug then Log.set_log_level Debug;
-    match (!command, List.rev !input_files) with
-    | (Dump | Process), [] -> raise Too_few_files
-    | Dump, _ :: _ :: _ -> raise Too_many_files
-    | Process, file :: build_path ->
-        let root = if !root = "." then None else Some !root in
-        Uideps.generate ~root ~output_file:!output_file ~build_path file
-    | Dump, [ file ] ->
-        File_format.(read ~file |> pp_payload Format.std_formatter)
-    | Aggregate, files ->
-        Uideps.aggregate ~store_shapes:!store_shapes ~output_file:!output_file files
-    | Usage, _ -> Format.print_string usage_msg
-  with
-  | Unknown_command cmd ->
-      Printf.eprintf "Unknown command %S." cmd;
-      exit 2
-  | Too_many_files ->
-      Printf.eprintf "Command %s expects exactly one input file."
-        (cmd_to_string !command);
-      exit 2
-  | Too_few_files ->
-      Printf.eprintf "Command %s requires at least one input file."
-        (cmd_to_string !command);
-      exit 2
+  let build_path =
+    let doc = "the extra build path" in
+    Arg.(value & pos_right 0 string [] & info [] ~doc)
+
+  let term =
+    Term.(const generate $ root $ Common.output_file $ build_path $ cmt_file)
+
+  let cmd =
+    let info =
+      let doc = "builds the index for a single $(i, .cmt) file" in
+      Cmd.info "process-cmt" ~doc
+    in
+    Cmd.v info (Common.with_log term)
+end
+
+module Aggregate = struct
+  let aggregate output_file store_shapes files =
+    Uideps.aggregate ~store_shapes ~output_file files
+
+  let store_shapes =
+    let doc =
+      "aggregate input-indexes shapes and store them in the new index"
+    in
+    Arg.(value & flag & info [ "store_shapes" ] ~doc)
+
+  let indexes =
+    let doc = "the indexes to aggregate" in
+    Arg.(non_empty & pos_all string [] & info [] ~doc)
+
+  let term =
+    Term.(const aggregate $ Common.output_file $ store_shapes $ indexes)
+
+  let cmd =
+    let info =
+      let doc = "merge multiple indexes into a unique one" in
+      Cmd.info "aggregate" ~doc
+    in
+    Cmd.v info (Common.with_log term)
+end
+
+module Dump = struct
+  let dump file = File_format.(read ~file |> pp_payload Format.std_formatter)
+
+  let file =
+    let doc = "the file to dump" in
+    Arg.(required & pos 0 (some string) None & info [] ~doc)
+
+  let term = Term.(const dump $ file)
+
+  let cmd =
+    let info =
+      let doc = "print the content of an index file to stdout" in
+      Cmd.info "dump" ~doc
+    in
+    Cmd.v info term
+end
+
+let subcommands =
+  let info =
+    let doc = "An indexer for OCaml's artifacts" in
+    Cmd.info "ocaml-uideps" ~doc
+  in
+  Cmd.group info [ Index.cmd; Aggregate.cmd; Dump.cmd ]
+
+let () = exit (Cmd.eval subcommands)
